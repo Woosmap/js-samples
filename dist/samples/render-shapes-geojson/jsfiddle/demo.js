dcea1ecd667d9c0acae27901851d5e3e7d116cd4
wgsadmin <operations@webgeoservices.com>
2024-02-07 14:49:34 +0000

    chore: update dist folder [skip ci]

---
+++ dist/samples/render-shapes-geojson/jsfiddle/demo.js
@@ -0,0 +1,51 @@
+// Initialize and add the map
+let map;
+
+function initMap() {
+  map = new window.woosmap.map.Map(document.getElementById("map"), {
+    center: { lat: 53.51, lng: 8.15 },
+    zoom: 4,
+  });
+  map.data.loadGeoJson(
+    "https://demo.woosmap.com/misc/data/europe.geojson.json",
+  );
+
+  const defaultStyle = {
+    fillColor: "#FFAB00",
+    strokeWeight: 2,
+  };
+
+  map.data.setStyle((feature) => {
+    let color = defaultStyle.fillColor;
+
+    if (feature.getProperty("highlighted")) {
+      color = "#C51162";
+    }
+    return {
+      fillColor: color,
+      strokeColor: color,
+      strokeOpacity: 1,
+      strokeWeight: defaultStyle.strokeWeight,
+    };
+  });
+
+  let highlightedFeatureId;
+
+  if (map && map.data) {
+    map.data.addListener("click", (event) => {
+      if (highlightedFeatureId) {
+        const highlightedFeature =
+          map.data.getFeatureById(highlightedFeatureId);
+
+        if (highlightedFeature) {
+          highlightedFeature.setProperty("highlighted", false);
+        }
+      }
+
+      event.feature.setProperty("highlighted", true);
+      highlightedFeatureId = event.feature.getId();
+    });
+  }
+}
+
+window.initMap = initMap;

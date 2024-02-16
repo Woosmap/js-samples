f150f7bd8ed79a3389fc0eddf77f8368a51f3cef
shwetadeshmukh <hgranger22@gmail.com>
2024-02-16 17:28:40 +0530

    Feat : Indoor Samples - Indoor Map Renderer - Indoor Widget - Indoor Directions Service - Indoor Navigation Widget

---
+++ samples/indoor-map-simple/index.ts
@@ -0,0 +1,47 @@
+// [START woosmap_indoor_map_simple]
+let map: woosmap.map.Map;
+
+function initMap() {
+  map = new window.woosmap.map.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      center: { lat: 43.6066, lng: 3.9218 },
+      zoom: 19.5,
+    },
+  );
+
+  const indoorRenderer = new woosmap.map.IndoorRenderer({
+    venue: "wgs_doc",
+    defaultFloor: 3,
+  });
+
+  // Indoor event that is triggered when the indoor venue is loaded.
+  indoorRenderer.addListener(
+    "indoor_venue_loaded",
+    (venue: woosmap.map.Venue) => {
+      console.log(venue);
+      hideLoader();
+    },
+  );
+  indoorRenderer.setMap(map);
+}
+const hideLoader = () => {
+  const loader = document.querySelector("#loader") as HTMLDivElement;
+  const mapContainer = document.querySelector("#map") as HTMLDivElement;
+  if (loader) {
+    loader.style.display = "none";
+  }
+  if (mapContainer) {
+    mapContainer.style.visibility = "visible";
+  }
+};
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
+// [END woosmap_indoor_map_simple]
+
+export {};
---
+++ samples/indoor-navigation-widget/index.ts
@@ -0,0 +1,77 @@
+// [START woosmap_indoor_navigation_widget]
+let map: woosmap.map.Map;
+let indoorRenderer: woosmap.map.IndoorRenderer;
+let indoorNavigation: woosmap.map.NavigationWidget;
+let indoorService: woosmap.map.IndoorService;
+let venue: string;
+
+function initMap(): void {
+  venue = "gdn_doc";
+  map = new window.woosmap.map.Map(
+    document.getElementById("map") as HTMLElement,
+    { center: { lat: 43.6066, lng: 3.9218 } },
+  );
+
+  const conf: woosmap.map.IndoorRendererOptions = {
+    venue: venue,
+    responsive: "desktop",
+  };
+
+  const indoorRenderer = new woosmap.map.IndoorRenderer(conf);
+
+  indoorRenderer.setMap(map);
+
+  // Indoor event that is triggered when the indoor venue is loaded.
+  indoorRenderer.addListener("indoor_venue_loaded", (venue) => {
+    console.log("Venue: ", venue);
+
+    indoorService = new window.woosmap.map.IndoorService();
+
+    const request: woosmap.map.IndoorDirectionRequest = {
+      venueId: "gdn_doc",
+      origin: new window.woosmap.map.LatLng(48.8801287, 2.3548678),
+      originLevel: 0,
+      destination: new window.woosmap.map.LatLng(48.8799341, 2.3563779),
+      destinationLevel: 0,
+      units: "metric",
+      originId: null,
+      destinationId: null,
+    };
+    indoorService.directions(request, (response) => {
+      if (response) {
+        indoorRenderer.setDirections(response);
+        indoorNavigation = new window.woosmap.map.NavigationWidget(
+          indoorRenderer,
+          exitNavigation,
+        );
+        indoorNavigation.setMap(map);
+        hideLoader();
+      }
+    });
+  });
+}
+const exitNavigation = () => {
+  indoorNavigation.setMap(null);
+  indoorNavigation = null;
+  indoorRenderer.setDirections(null);
+};
+const hideLoader = () => {
+  const loader = document.querySelector("#loader") as HTMLDivElement;
+  const mapContainer = document.querySelector("#map") as HTMLDivElement;
+  if (loader) {
+    loader.style.display = "none";
+  }
+  if (mapContainer) {
+    mapContainer.style.visibility = "visible";
+  }
+};
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
+// [END woosmap_indoor_navigation_widget]
+
+export {};

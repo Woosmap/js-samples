4afeb57860b2d0153d4799f22b798edafc74b7cf
galela <gsimon@woosmap.com>
2024-02-28 17:02:51 +0100

    feat: Added Map Clustering sample

--- samples/samples.11tydata.js
+++ samples/samples.11tydata.js
@@ -10,7 +10,7 @@ const data = {
   ),
   libraries: [],
   mode: ["jsfiddle", "docs", "app", "highlight"],
-  availableTypings: ["@types/woosmap.map"],
+  availableTypings: ["@types/woosmap.map", "@types/geojson"],
   devDependencies: ["@types/woosmap.map", "typescript", "vite"],
   dependencies: [],
   scripts: {

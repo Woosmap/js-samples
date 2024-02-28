b3fa2cb88a491acb1ad42cf2125fe63e69aa22b5
shwetadeshmukh <hgranger22@gmail.com>
2024-02-28 12:03:50 +0530

    Refactored - Localities autocomplete request sample

--- samples/localities-autocomplete-requests/index.ts
+++ samples/localities-autocomplete-requests/index.ts
@@ -1,92 +1,114 @@
 // [START woosmap_localities_autocomplete_requests]
 const componentsRestriction = [];
-const woosmap_key = "woos-48c80350-88aa-333e-835a-07f4b658a9a4";
+const woosmap_key = "YOUR_API_KEY";
 
-function displayAddress() {
-  const inputElement: HTMLInputElement = document.getElementById(
-    "input",
-  ) as HTMLInputElement;
-  let value = "";
-  if (inputElement) {
-    value = inputElement.value;
+const inputElement = document.getElementById(
+  "autocomplete-input",
+) as HTMLInputElement;
+const suggestionsList = document.getElementById(
+  "suggestions-list",
+) as HTMLUListElement;
+const clearSearchBtn = document.getElementsByClassName(
+  "clear-searchButton",
+)[0] as HTMLButtonElement;
+const responseElement = document.getElementById(
+  "selected-locality",
+) as HTMLElement;
+
+function init(): void {
+  if (inputElement && suggestionsList) {
+    inputElement.addEventListener("input", handleAutocomplete);
+    inputElement.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        const firstLi = suggestionsList.querySelector("li");
+        if (firstLi) {
+          firstLi.click();
+        }
+      }
+    });
   }
-
-  const results: HTMLElement = document.querySelector(
-    ".autocomplete-results",
-  ) as HTMLElement;
-
-  const components: string[] = componentsRestriction.map(
-    ({ id }) => `country:${id}`,
-  );
-  const componentsArgs: string = components.join("|");
-
-  autocompleteAddress(value, componentsArgs, woosmap_key).then(
-    ({ localities }) => {
-      if (results && results.parentElement) {
-        results.innerHTML = "";
-        results.parentElement.style.display = "none";
-      }
-      let html = "";
-      for (const prediction_id in localities || []) {
-        const prediction = localities[prediction_id];
-        const predictionClass = "no-viewpoint";
-
-        const matched_substrings = prediction.matched_substrings;
-        let formatted_name = "";
-        if (
-          prediction.matched_substrings &&
-          prediction.matched_substrings.description
-        ) {
-          formatted_name = bold_matched_substring(
-            prediction["description"],
-            matched_substrings.description,
-          );
-        } else {
-          formatted_name = prediction["description"];
-        }
-
-        html += `<div class="prediction ${predictionClass}" prediction-id=${prediction_id}>${formatted_name}</div>`;
-      }
-      if (results && results.parentElement) {
-        results.innerHTML = html;
-        results.style.display = "block";
-        results.parentElement.style.display = "flex";
-      }
-
-      const data = results.querySelectorAll(".prediction");
-
-      for (const result of data) {
-        if (result && results && results.parentElement) {
-          result.addEventListener("click", () => {
-            results.style.display = "none";
-            if (results.parentElement) {
-              results.parentElement.style.display = "none";
-            }
-            const predictictionVal = result.getAttribute("prediction-id");
-            if (predictictionVal) {
-              const predictionId = parseInt(predictictionVal, 10);
-              const prediction = localities[predictionId];
-              const inputElement = document.getElementById(
-                "input",
-              ) as HTMLInputElement;
-              if (inputElement) {
-                inputElement.value = prediction.description;
-              }
-              const selectedElement = document.getElementById(
-                "selected-locality",
-              ) as HTMLElement;
-              if (selectedElement) {
-                selectedElement.innerHTML = `<code>${JSON.stringify(prediction, null, 2)}</code>`;
-              }
-            }
-          });
-        }
-      }
-    },
-  );
+  clearSearchBtn.addEventListener("click", () => {
+    inputElement.value = "";
+    suggestionsList.style.display = "none";
+    clearSearchBtn.style.display = "none";
+    responseElement.innerHTML = "";
+    inputElement.focus();
+  });
 }
 
-function bold_matched_substring(string, matched_substrings) {
+function handleAutocomplete(): void {
+  if (inputElement && suggestionsList) {
+    const input = inputElement.value;
+    input.replace('"', '\\"').replace(/^\s+|\s+$/g, "");
+    const components: string[] = componentsRestriction.map(
+      ({ id }) => `country:${id}`,
+    );
+    const componentsArgs: string = components.join("|");
+    if (input !== "") {
+      const debounceRequest = debounce(() => {
+        autocompleteAddress(input, componentsArgs, woosmap_key)
+          .then(({ localities }) => displaySuggestions(localities))
+          .catch((error) =>
+            console.error("Error autocomplete localities:", error),
+          );
+      }, 0);
+      debounceRequest();
+    }
+  }
+}
+function displaySuggestions(
+  localitiesPredictions: woosmap.map.localities.LocalitiesPredictions[],
+) {
+  if (inputElement && suggestionsList) {
+    suggestionsList.innerHTML = "";
+    if (localitiesPredictions.length > 0) {
+      localitiesPredictions.forEach((locality) => {
+        const li = document.createElement("li");
+        li.innerHTML = formatPredictionList(locality) ?? "";
+        li.addEventListener("click", () => {
+          inputElement.value = locality.description ?? "";
+          suggestionsList.style.display = "none";
+          displayLocalitiesResponse(locality);
+        });
+        suggestionsList.appendChild(li);
+      });
+      suggestionsList.style.display = "block";
+      clearSearchBtn.style.display = "block";
+    } else {
+      suggestionsList.style.display = "none";
+    }
+  }
+}
+function formatPredictionList(locality): string {
+  const prediction = locality;
+  const predictionClass = "no-viewpoint";
+  const matched_substrings = prediction.matched_substrings;
+  let formatted_name = "";
+  if (
+    prediction.matched_substrings &&
+    prediction.matched_substrings.description
+  ) {
+    formatted_name = bold_matched_substring(
+      prediction["description"],
+      matched_substrings.description,
+    );
+  } else {
+    formatted_name = prediction["description"];
+  }
+
+  let html = "";
+  html += `<div class="prediction ${predictionClass}">${formatted_name}</div>`;
+
+  return html;
+}
+function displayLocalitiesResponse(
+  selectedLocality: woosmap.map.localities.LocalitiesPredictions,
+) {
+  if (responseElement) {
+    responseElement.innerHTML = `<code>${JSON.stringify(selectedLocality, null, 2)}</code>`;
+  }
+}
+function bold_matched_substring(string: string, matched_substrings: string[]) {
   matched_substrings = matched_substrings.reverse();
   for (const substring of matched_substrings) {
     const char = string.substring(
@@ -102,35 +124,11 @@ function bold_matched_substring(string, matched_substrings) {
   }
   return string;
 }
-
-function initUI() {
-  const results = document.querySelector(
-    ".autocomplete-results",
-  ) as HTMLElement;
-  const input = document.querySelector(
-    ".autocomplete-input > input",
-  ) as HTMLInputElement;
-
-  input?.addEventListener(
-    "input",
-    debounce(() => {
-      const value = input.value;
-      value.replace('"', '\\"').replace(/^\s+|\s+$/g, "");
-      if (value !== "") {
-        displayAddress();
-      } else {
-        if (results) {
-          results.innerHTML = "";
-        }
-      }
-    }, 0),
-  );
-}
 function autocompleteAddress(
   input: string,
   components: string,
   woosmap_key: string,
-) {
+): Promise<woosmap.map.localities.LocalitiesAutocompleteResponse> {
   const args = {
     key: woosmap_key,
     input,
@@ -186,14 +184,14 @@ function debounce(func: FuncType, wait: number, immediate?: boolean): FuncType {
   };
 }
 
-initUI();
+init();
 
 declare global {
   interface Window {
-    initUI: () => void;
+    init: () => void;
   }
 }
-window.initUI = initUI;
+window.init = init;
 // [END woosmap_localities_autocomplete_requests]
 
 export {};

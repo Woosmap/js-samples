1e70d4de7f0503b0d3b40947af5868dd8ea4ab17
galela <gsimon@woosmap.com>
2024-05-15 08:29:38 +0200

    feat: used MapJS localitiesService.nearby method to query localities nearby

--- samples/localities-nearby-poi/index.ts
+++ samples/localities-nearby-poi/index.ts
@@ -1,6 +1,64 @@
 // [START woosmap_localities_nearby_poi]
-// Initialize and add the map
-const woosmap_key = "YOUR_API_KEY";
+const availableCategories = [
+  "business",
+  "business.cinema",
+  "business.theatre",
+  "business.nightclub",
+  "business.finance",
+  "business.finance.bank",
+  "business.fuel",
+  "business.parking",
+  "business.mall",
+  "business.food_and_drinks",
+  "business.food_and_drinks.bar",
+  "business.food_and_drinks.biergarten",
+  "business.food_and_drinks.cafe",
+  "business.food_and_drinks.fast_food",
+  "business.food_and_drinks.pub",
+  "business.food_and_drinks.restaurant",
+  "business.food_and_drinks.food_court",
+  "business.shop",
+  "business.shop.mall",
+  "business.shop.bakery",
+  "business.shop.butcher",
+  "business.shop.library",
+  "business.shop.grocery",
+  "business.shop.sports",
+  "business.shop.toys",
+  "business.shop.clothes",
+  "business.shop.furniture",
+  "business.shop.electronics",
+  "education",
+  "education.school",
+  "education.kindergarten",
+  "education.university",
+  "education.college",
+  "education.library",
+  "hospitality",
+  "hospitality.hotel",
+  "hospitality.hostel",
+  "hospitality.guest_house",
+  "hospitality.bed_and_breakfast",
+  "hospitality.motel",
+  "medical",
+  "medical.hospital",
+  "medical.pharmacy",
+  "medical.clinic",
+  "tourism",
+  "tourism.attraction",
+  "tourism.attraction.amusement_park",
+  "tourism.attraction.zoo",
+  "tourism.attraction.aquarium",
+  "tourism.monument",
+  "tourism.monument.castle",
+  "tourism.museum",
+  "government",
+  "park",
+  "place_of_worship",
+  "police",
+  "post_office",
+  "sports",
+];
 const categories: Set<string> = new Set();
 let map: woosmap.map.Map;
 let results: HTMLOListElement;
@@ -8,25 +66,63 @@ let nearbyCircle: woosmap.map.Circle;
 let marker: woosmap.map.Marker;
 let localitiesService: woosmap.map.LocalitiesService;
 let request: woosmap.map.localities.LocalitiesAutocompleteRequest;
-const nearbyParameters={
-    types: "point_of_interest",
-    location: "40.71399,-74.00499",
-    radius: 1000,
-    categories: "",
-    page_number: 1,
-    page_size: 10
-  };
-let debouncedLocalitiesAutocomplete: (
-  ...args: any[]
-) => Promise<woosmap.map.localities.LocalitiesAutocompleteResponse>;
-let debouncedNearby: (...args: any[]) => Promise<any>;
+const nearbyParameters = {
+  types: "point_of_interest",
+  location: {},
+  radius: 1000,
+  categories: "",
+  page: 1,
+  limit: 10,
+};
+
+const buildTree = (availableCategories: string[]) => {
+  const tree = {};
+  availableCategories.forEach((category) => {
+    const parts = category.split(".");
+    let node = tree;
+    parts.forEach((part) => {
+      node[part] = node[part] || {};
+      node = node[part];
+    });
+  });
+  return tree;
+};
+
+const createList = (node: any, prefix = "") => {
+  const ul = document.createElement("ul");
+  for (const key in node) {
+    const fullKey = prefix ? `${prefix}.${key}` : key;
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = `category-${fullKey}`;
+    checkbox.name = "categories";
+    checkbox.classList.add("category");
+    checkbox.value = fullKey;
+
+    const label = document.createElement("label");
+    label.htmlFor = `category-${fullKey}`;
+    label.textContent = key;
+
+    li.appendChild(checkbox);
+    li.appendChild(label);
+
+    const children = createList(node[key], fullKey);
+    if (children) {
+      li.appendChild(children);
+    }
+    ul.appendChild(li);
+  }
+  return ul.childElementCount ? ul : null;
+};
 
 function initMap() {
   map = new window.woosmap.map.Map(
     document.getElementById("map") as HTMLElement,
     {
       center: { lat: 40.71399, lng: -74.00499 },
-      zoom: 15,
+      zoom: 14,
       styles: [
         {
           featureType: "point_of_interest",
@@ -40,18 +136,12 @@ function initMap() {
       ],
     },
   );
-  localitiesService = new window.woosmap.map.LocalitiesService();
+  localitiesService = new woosmap.map.LocalitiesService();
 
   request = {
     input: "",
     types: ["locality", "postal_code"],
   };
-
-  debouncedLocalitiesAutocomplete = debouncePromise(
-    localitiesService.autocomplete,
-    0,
-  );
-  debouncedNearby = debouncePromise(fetch, 500);
 
   marker = new woosmap.map.Marker({
     position: { lat: 0, lng: 0 },
@@ -68,73 +158,104 @@ function initMap() {
   performNearbyRequest();
 }
 
-function initUI() {
-  results = document.querySelector("#results") as HTMLOListElement;
+function buildCategoriesList() {
+  const tree = buildTree(availableCategories);
+  const list = createList(tree);
+  (
+    document.querySelector(".categoriesOptions__list") as HTMLDivElement
+  ).appendChild(list as HTMLElement);
   document.querySelectorAll(".category").forEach((el) =>
     el.addEventListener("click", (ev) => {
       const inputElement = ev.target as HTMLInputElement;
+      const parentLi = inputElement.closest("li");
+      const childrenCheckboxes = parentLi
+        ? Array.from(parentLi.children)
+            .filter((child) => child !== inputElement)
+            .flatMap((child) => Array.from(child.querySelectorAll(".category")))
+        : [];
+
       if (inputElement.checked) {
         categories.add(inputElement.value);
+        if (childrenCheckboxes.length > 0) {
+          childrenCheckboxes.forEach((checkbox) => {
+            (checkbox as HTMLInputElement).disabled = true;
+          });
+        }
       } else {
         categories.delete(inputElement.value);
+        if (childrenCheckboxes.length > 0) {
+          childrenCheckboxes.forEach((checkbox) => {
+            (checkbox as HTMLInputElement).disabled = false;
+          });
+        }
       }
       performNearbyRequest();
     }),
   );
+}
+
+function handleRadius(
+  radiusValue: number,
+  center?: woosmap.map.LatLng | woosmap.map.LatLngLiteral | null,
+) {
+  const label = document.getElementById("radius-label");
+  if (radiusValue < 1000 && label) {
+    label.innerHTML = `${radiusValue}&thinsp;m`;
+  } else if (label) {
+    label.innerHTML = `${radiusValue / 1000}&thinsp;km`;
+  }
+  // circle.getBounds() returns wrong LatLngBounds -> const bounds = nearbyCircle.getBounds();
+  // TODO fixed circle getBounds
+  // used this log scale to compute the zoom level between z18 (radius 10m) and z7 (radius 50km)
+  const zoomLevel = Math.round(
+    18 - (Math.log(radiusValue / 10) / Math.log(50000 / 10)) * (18 - 7),
+  );
+  map.flyTo({ center: center || nearbyCircle.getCenter(), zoom: zoomLevel });
+  performNearbyRequest();
+}
+
+function initUI() {
+  results = document.querySelector("#results") as HTMLOListElement;
+  buildCategoriesList();
+  const debouncedHandleRadius = debounce(handleRadius, 300);
 
   document.getElementById("radius")?.addEventListener("input", (e) => {
-    const value = parseInt((e.target as HTMLInputElement).value);
-    const label = document.getElementById("radius-label");
-    if (value < 1000 && label) {
-      label.innerHTML = `${value}&thinsp;m`;
-    } else if (label) {
-      label.innerHTML = `${value / 1000}&thinsp;km`;
-    }
-    // The following fails, displaying bounds show no difference betwee range min and max
-    // map.fitBounds(nearbyCircle.getBounds(), {top:80});
-    performNearbyRequest();
+    const radiusValue = parseInt((e.target as HTMLInputElement).value);
+    debouncedHandleRadius(radiusValue);
   });
-
   document.getElementById("page-previous")?.addEventListener("click", () => {
-    nearbyParameters.page_number--;
+    nearbyParameters.page--;
     performNearbyRequest(null, false);
   });
-
   document.getElementById("page-next")?.addEventListener("click", () => {
-    nearbyParameters.page_number++;
-    performNearbyRequest(null,false);
+    nearbyParameters.page++;
+    performNearbyRequest(null, false);
   });
-
 }
 
 function performNearbyRequest(
   overrideCenter: woosmap.map.LatLng | null = null,
-  newQuery:boolean=true
+  newQuery = true,
 ) {
-  const requestCenter = overrideCenter ? overrideCenter : map.getCenter();
+  const requestCenter = overrideCenter || map.getCenter();
   const radiusElement = document.getElementById("radius") as HTMLInputElement;
-  nearbyParameters.location = `${requestCenter.lat()},${requestCenter.lng()}`;
-  nearbyParameters.radius = radiusElement ? parseInt(radiusElement.value) : 1000;
+  nearbyParameters.location = requestCenter;
+  nearbyParameters.radius = radiusElement
+    ? parseInt(radiusElement.value)
+    : 1000;
   nearbyParameters.categories = "";
   if (categories.size > 0) {
     nearbyParameters.categories = Array.from(categories).join("|");
   }
-  if(newQuery){
-    nearbyParameters.page_number=1
+  if (newQuery) {
+    nearbyParameters.page = 1;
   }
 
-  debouncedNearby(
-    `//api.woosmap.com/localities/nearby?key=${woosmap_key}&${buildQueryString(nearbyParameters)}`
-  )
-    .then((response) => {
-      if (response.status == 200) {
-        return response.json();
-      }
-    })
-    .then((responseJson) => {
-      drawNearbyZone(requestCenter, nearbyParameters.radius);
-      updateResults(responseJson, requestCenter);
-    });
+  //@ts-ignore
+  localitiesService.nearby(nearbyParameters).then((responseJson) => {
+    drawNearbyZone(requestCenter, nearbyParameters.radius);
+    updateResults(responseJson, requestCenter);
+  });
 }
 
 function drawNearbyZone(center, radius) {
@@ -153,26 +274,23 @@ function drawNearbyZone(center, radius) {
   });
 }
 
-function updatePagination(hasNextPage:boolean){
-  if(hasNextPage){
-    document.getElementById("page-next")?.removeAttribute("disabled")
+function updatePagination(hasNextPage: boolean) {
+  if (hasNextPage) {
+    document.getElementById("page-next")?.removeAttribute("disabled");
+  } else {
+    document.getElementById("page-next")?.setAttribute("disabled", "true");
   }
-  else{
-    document.getElementById("page-next")?.setAttribute("disabled", "true")
+  if (nearbyParameters.page > 1) {
+    document.getElementById("page-previous")?.removeAttribute("disabled");
+  } else {
+    document.getElementById("page-previous")?.setAttribute("disabled", "true");
   }
-  if(nearbyParameters.page_number > 1){
-    document.getElementById("page-previous")?.removeAttribute("disabled")
-  }
-  else{
-    document.getElementById("page-previous")?.setAttribute("disabled", "true")
-  }
-
 }
 
 function updateResults(response, center) {
   results.innerHTML = "";
 
-  updatePagination(response["pagination"]["has_next_page"])
+  updatePagination(response["pagination"]["next_page"]);
   response["results"].forEach((result) => {
     const distance = measure(
       center.lat(),
@@ -253,7 +371,8 @@ function handleAutocomplete(): void {
   if (inputElement && suggestionsList) {
     request.input = inputElement.value;
     if (request.input) {
-      debouncedLocalitiesAutocomplete(request)
+      localitiesService
+        .autocomplete(request)
         .then((localities) => displaySuggestions(localities))
         .catch((error) =>
           console.error("Error autocomplete localities:", error),
@@ -276,8 +395,8 @@ function displayLocality(
   locality: woosmap.map.localities.LocalitiesDetailsResult,
 ) {
   if (locality?.geometry) {
-    map.flyTo({ center: locality.geometry.location, zoom: 14, duration: 3 });
-    performNearbyRequest(new woosmap.map.LatLng(locality.geometry.location));
+    map.setCenter(locality.geometry.location);
+    handleRadius(nearbyParameters.radius, locality.geometry.location);
   }
 }
 
@@ -316,53 +435,15 @@ document.addEventListener("click", (event) => {
   }
 });
 
-function buildQueryString(params: object) {
-  const queryStringParts = [];
-
-  for (const key in params) {
-    if (params[key]) {
-      const value = params[key];
-      queryStringParts.push(
-        `${encodeURIComponent(key)}=${encodeURIComponent(value)}` as never,
-      );
-    }
-  }
-  return queryStringParts.join("&");
-}
-
-type DebouncePromiseFunction<T, Args extends any[]> = (
-  ...args: Args
-) => Promise<T>;
-
-function debouncePromise<T, Args extends any[]>(
-  fn: (...args: Args) => Promise<T>,
-  delay: number,
-): DebouncePromiseFunction<T, Args> {
-  let timeoutId: ReturnType<typeof setTimeout> | null = null;
-  let latestResolve: ((value: T | PromiseLike<T>) => void) | null = null;
-  let latestReject: ((reason?: any) => void) | null = null;
-
-  return function (...args: Args): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-      if (timeoutId !== null) {
-        clearTimeout(timeoutId);
-      }
-      latestResolve = resolve;
-      latestReject = reject;
-      timeoutId = setTimeout(() => {
-        fn(...args)
-          .then((result) => {
-            if (latestResolve === resolve && latestReject === reject) {
-              resolve(result);
-            }
-          })
-          .catch((error) => {
-            if (latestResolve === resolve && latestReject === reject) {
-              reject(error);
-            }
-          });
-      }, delay);
-    });
+function debounce(func: (...args: any[]) => void, wait: number) {
+  let timeout: NodeJS.Timeout;
+  return function executedFunction(...args: any[]) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
   };
 }
 

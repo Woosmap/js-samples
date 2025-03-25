7719f5a4ef7fabcf7da84858a0ecd49feb5876f3
christopher dartnell <cdartnell@woosmap.com>
2025-03-25 12:59:15 +0100

    adapt geocode demo to hit both prod and PR deploy and show both results

--- samples/localities-geocode/index.ts
+++ samples/localities-geocode/index.ts
@@ -1,8 +1,13 @@
 // [START woosmap_localities_geocode]
 // Initialize and add the map
+import LocalitiesGeocodeRequest = woosmap.map.localities.LocalitiesGeocodeRequest;
+
 let map: woosmap.map.Map;
 let marker: woosmap.map.Marker;
 let infoWindow: woosmap.map.InfoWindow;
+let pr_marker: woosmap.map.Marker;
+let pr_infoWindow: HTMLElement | null;
+let PR_NUMBER = "1216"
 let localitiesService: woosmap.map.LocalitiesService;
 const request: woosmap.map.localities.LocalitiesGeocodeRequest = {};
 
@@ -15,6 +20,7 @@ function initMap() {
     },
   );
   infoWindow = new woosmap.map.InfoWindow({});
+  pr_infoWindow = document.getElementById("instructions")
   localitiesService = new woosmap.map.LocalitiesService();
   map.addListener("click", (e) => {
     handleGeocode(e.latlng);
@@ -49,8 +55,48 @@ clearSearchBtn.addEventListener("click", () => {
     marker.setMap(null);
     infoWindow.close();
   }
+  if (pr_marker) {
+    pr_marker.setMap(null);
+  }
   inputElement.focus();
 });
+
+function buildQueryString(params: object) {
+  const queryStringParts = [];
+
+  for (const key in params) {
+    if (params[key]) {
+      const value = params[key];
+      queryStringParts.push(
+        `${encodeURIComponent(key)}=${encodeURIComponent(value)}` as never,
+      );
+    }
+  }
+  return queryStringParts.join("&");
+}
+
+const pr_reverse_geocode = async (latLng:woosmap.map.LatLngLiteral|woosmap.map.LatLng): Promise<any> => {
+  let params = {
+    "latlng": `${latLng.lat},${latLng.lng}`,
+    "key": "woos-b2f35903-92d8-3a95-9b35-dd503c752a51"
+  }
+  if(request.components) {
+    params["components"] = (request.components.country as string[])
+        .map((country) => `country:${country}`)
+        .join("|");
+  }
+
+  try {
+    const response = await fetch(
+      `
+https://develop-api.woosmap.com/${PR_NUMBER}/localities/geocode?${buildQueryString(params)}`
+    );
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching PR:", error);
+    throw error;
+  }
+};
 
 function handleGeocode(latlng: woosmap.map.LatLngLiteral | null) {
   if (latlng) {
@@ -66,6 +112,10 @@ function handleGeocode(latlng: woosmap.map.LatLngLiteral | null) {
       .geocode(request)
       .then((localities) => displayLocality(localities.results[0]))
       .catch((error) => console.error("Error geocoding localities:", error));
+    if (request.latLng) {
+      pr_reverse_geocode(request.latLng).then((localities) => displayPRLocality(localities.results[0]))
+      .catch((error) => console.error("Error geocoding localities:", error));
+    }
   }
 }
 
@@ -92,7 +142,35 @@ function displayLocality(
     infoWindow.setContent(`<span>${locality.formatted_address}</span>`);
     infoWindow.open(map, marker);
     map.setCenter(locality.geometry.location);
-    map.setZoom(14);
+    if (map.getZoom() < 14) {
+      map.setZoom(14);
+    }
+  }
+}
+
+function displayPRLocality(
+  locality: woosmap.map.localities.LocalitiesGeocodeResult | null,
+) {
+  if (pr_marker) {
+    pr_marker.setMap(null);
+  }
+
+  if (locality?.geometry) {
+    pr_marker = new woosmap.map.Marker({
+      position: locality.geometry.location,
+      icon: {
+        url: "https://images.woosmap.com/marker-red.png",
+        scaledSize: {
+          height: 50,
+          width: 32,
+        },
+      },
+    });
+    pr_marker.setMap(map);
+    if (pr_infoWindow) {
+      pr_infoWindow.textContent = `[PR says] ${locality.formatted_address} (red marker)`;
+    }
+
   }
 }
 

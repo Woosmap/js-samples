2c30ce7d045ad9f5b3956c531bab8e806966edfe
galela <gsimon@woosmap.com>
2025-11-27 16:47:03 +0100

    feat: update release workflow

--- sample-to-branch.sh
+++ sample-to-branch.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 set -e
 
+# Suppress git init warnings
+git config --global init.defaultBranch master
+
 tmp=`mktemp -d`
 
 for sample in dist/samples/*/; do
   name=`basename $sample`
   branch="sample/${name}"
 
+  echo "Processing $name to branch $branch..."
+
   pushd $tmp
     rm -rf .git
     git init
-    git config user.name 'gaelsimon'
-    git config user.email 'gael.simon@woosmap.com'
+
+    # Using wgsadmin as requested
+    git config user.name 'wgsadmin'
+    git config user.email 'operations@webgeoservices.com'
 
     if [ -n "$GITHUB_TOKEN" ]; then
-      git remote add origin "https://gaelsimon:$GITHUB_TOKEN@github.com/woosmap/js-samples.git"
+      # Authenticated remote using the token passed from the workflow
+      git remote add origin "https://x-access-token:$GITHUB_TOKEN@github.com/woosmap/js-samples.git"
     else
+      # Fallback for local testing
       git remote add origin "git@github.com:woosmap/js-samples.git"
     fi
 
@@ -26,16 +35,14 @@ for sample in dist/samples/*/; do
 
   cp -r $sample/app/* $tmp
 
-  # manually copy all files beginning with .
-  cp $sample/app/.env $tmp/.env
-  cp $sample/app/.eslintrc.json $tmp/.eslintrc.json
-  cp $sample/app/.gitignore $tmp/.gitignore
-
+  # Manually copy config files
+  [ -f "$sample/app/.env" ] && cp "$sample/app/.env" "$tmp/.env"
+  [ -f "$sample/app/.eslintrc.json" ] && cp "$sample/app/.eslintrc.json" "$tmp/.eslintrc.json"
+  [ -f "$sample/app/.gitignore" ] && cp "$sample/app/.gitignore" "$tmp/.gitignore"
 
   pushd $tmp
     git add -A
     git commit -am "chore: sync ${name} [skip-ci]" --no-verify || true
     git push -q --set-upstream origin $branch -f
-    git status
   popd
 done

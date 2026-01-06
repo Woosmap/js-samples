36d2dfcbf98f6e1bab65b4ebe8ea6d30b703d7c3
wgsadmin <operations@webgeoservices.com>
2026-01-06 12:45:39 +0000

    chore: sync marker-simple [skip-ci]

---
+++ index.ts
@@ -0,0 +1,31 @@
+function initMap(): void {
+  const center: woosmap.map.LatLngLiteral = { lat: 51.52, lng: -0.13 };
+
+  const map = new woosmap.map.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 13,
+      center: center,
+    },
+  );
+
+  const marker = new woosmap.map.Marker({
+    position: map.getCenter(),
+    icon: {
+      url: "https://images.woosmap.com/marker.png",
+      scaledSize: {
+        height: 50,
+        width: 32,
+      },
+    },
+  });
+  marker.setMap(map);
+}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
+export {};

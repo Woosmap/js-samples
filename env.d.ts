f68fc63b17743c94ce46579f1dc56aae33796bf0
wgsadmin <operations@webgeoservices.com>
2026-09-04 09:09:22 +0000

    chore: sync indoor-navigation-widget [skip-ci]

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+/**
+ *  @external https://vitejs.dev/guide/env-and-mode.html
+ */
+interface ImportMetaEnv {
+  readonly VITE_WOOSMAP_PUBLIC_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
